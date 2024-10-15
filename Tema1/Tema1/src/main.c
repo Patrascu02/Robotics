a73405836c53fc3f4f876ac8691354320cb04c0e
KanekiLor <66566101+KanekiLor@users.noreply.github.com>
2024-10-15 19:44:05 +0300

    Tema1
    Mai multe detalii gasiti in README

---
+++ Tema1/Tema1/src/main.c
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+void setup() {
+	pinMode(13,OUTPUT);
+	pinMode(12,OUTPUT);
+	pinMode(11,OUTPUT);
+	pinMode(10,OUTPUT);
+	pinMode(9,OUTPUT);
+	pinMode(8,OUTPUT);
+	pinMode(7,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(5,OUTPUT);
+	pinMode(3,INPUT_PULLUP);
+    pinMode(2,INPUT_PULLUP);
+
+}
+unsigned int o=10,ok=0;
+void loop() {
+    if(o==10)
+    digitalWrite(5,HIGH);
+if(digitalRead(2) == LOW){ 
+    digitalWrite(5,LOW);            //Led-ul RGB devine rosu cand incepe procesul
+    digitalWrite(6,HIGH);
+    for (int i = 0; i < 4; i++) {
+        if(digitalRead(3) == LOW){
+            loop();
+        }
+       int q=0;
+       while(q<3 && o >=7){             //Incep led-urile sa se incarce
+        digitalWrite(o, HIGH);
+        delay(250);
+        digitalWrite(o, LOW);
+        delay(250);
+        q++;}
+        if(o >=7)
+            digitalWrite(o, HIGH  );        //Ramane ultimul led aprins
+        o--;
+
+        if(digitalRead(3) == LOW){          
+            delay(60);
+            if(digitalRead(3) == LOW){
+                ok=1;
+            }
+            
+        }
+    }   
+    if(ok==0){
+        for (int i = 0; i < 4; i++) {           //Led-urile incep toate sa se aprinda si stinga
+            digitalWrite(10,HIGH);
+            digitalWrite(9,HIGH);
+            digitalWrite(8,HIGH);
+            digitalWrite(7,HIGH);
+            delay(300);
+            digitalWrite(10,LOW);
+            digitalWrite(9,LOW);
+            digitalWrite(8,LOW);
+            digitalWrite(7,LOW);
+            delay(300);
+        }
+    }else{
+        digitalWrite(10,LOW);
+            digitalWrite(9,LOW);
+            digitalWrite(8,LOW);
+            digitalWrite(7,LOW);
+    }
+                                                //Led-ul RGB devine verde inapoi
+        digitalWrite(6,LOW);
+        digitalWrite(5,HIGH);
+}
+}
