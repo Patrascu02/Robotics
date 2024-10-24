9db33b20586bacebab9589251c8ee68cbcaa26fd
KanekiLor <66566101+KanekiLor@users.noreply.github.com>
2024-10-24 17:22:29 +0300

    Update main.c

--- Tema1/Tema1/src/main.c
+++ Tema1/Tema1/src/main.c
@@ -4,32 +4,35 @@ void setup() {
 	pinMode(9,OUTPUT);
 	pinMode(8,OUTPUT);
 	pinMode(7,OUTPUT);
-    pinMode(6,OUTPUT);
-    pinMode(5,OUTPUT);
+   	pinMode(6,OUTPUT);
+	pinMode(5,OUTPUT);
 	pinMode(3,INPUT_PULLUP);
-    pinMode(2,INPUT_PULLUP);
+    	pinMode(2,INPUT_PULLUP);
 
 }
 unsigned int o=10,ok=0;
 void loop() {
-    if(o==10)
+	
+if(o==10)
     digitalWrite(5,HIGH);
+
 if(digitalRead(2) == LOW){ 
     digitalWrite(5,LOW);            //Led-ul RGB devine rosu cand incepe procesul
     digitalWrite(6,HIGH);
-    for (int i = 0; i < 4; i++) {
-        if(digitalRead(3)==LOW){
-            o=10;i=0;ok=0;
-            break;
-        }
+        for (int i = 0; i < 4; i++) {
+            if(digitalRead(3)==LOW){
+                o=10;i=0;ok=0;
+                break;
+            }
        int q=0;
        while(q<3 && o >=7){             //Incep led-urile sa se incarce
-        digitalWrite(o, HIGH);
-        delay(250);
-        digitalWrite(o, LOW);
-        delay(250);
-        q++;
+            digitalWrite(o, HIGH);
+            delay(250);
+            digitalWrite(o, LOW);
+            delay(250);
+             q++;
         }
+        
         if(o >=7)
             digitalWrite(o, HIGH  );        //Ramane ultimul led aprins
         o--;
@@ -64,5 +67,7 @@ if(digitalRead(2) == LOW){
                                                 //Led-ul RGB devine verde inapoi
         digitalWrite(6,LOW);
         digitalWrite(5,HIGH);
+        o=10;
+        ok=0;
 }
 }
